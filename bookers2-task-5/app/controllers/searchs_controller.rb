6b303b8f0ccafd845394a059459d4a84c70ad1ff
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-02-25 09:23:24 +0000

    update

--- bookers2-task-5/app/controllers/searchs_controller.rb
+++ bookers2-task-5/app/controllers/searchs_controller.rb
@@ -1,26 +1,38 @@
 class SearchsController < ApplicationController
-  def search
-    @model = params["model"]
-    @content = params["content"]
-    @method = params["method"]
-    @records = search_for(@model, @content, @method)
-  end
+  before_action :authenticate_user!
+
+	def search
+		@model = params[:model]
+		@content = params[:content]
+		@method = params[:method]
+		if @model == 'user'
+			@records = User.search_for(@content, @method)
+		else
+			@records = Book.search_for(@content, @method)
+		end
+	end
+  # def search
+  #   @model = params["model"]
+  #   @content = params["content"]
+  #   @method = params["method"]
+  #   @records = search_for(@model, @content, @method)
+  # end
   
-  private
+  # private
   
-  def search_for(model, content, method)
-    if model == 'user'
-      if method == 'perfect'
-        User.where(name: content)
-      else
-        User.where('name LIKE ?', '%'+content+'%')
-      end
-    elsif model == 'book'
-      if method == 'perfect'
-        Book.where(title: content)
-      else
-        Book.where('title LIKE ?', '%'+content+'%')
-      end
-    end
-  end
+  # def search_for(model, content, method)
+  #   if model == 'user'
+  #     if method == 'perfect'
+  #       User.where(name: content)
+  #     else
+  #       User.where('name LIKE ?', '%'+content+'%')
+  #     end
+  #   elsif model == 'book'
+  #     if method == 'perfect'
+  #       Book.where(title: content)
+  #     else
+  #       Book.where('title LIKE ?', '%'+content+'%')
+  #     end
+  #   end
+  # end
 end
