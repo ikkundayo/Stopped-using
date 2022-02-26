38daa2201f455482a205409402ef5372350518d4
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-02-26 12:00:48 +0000

    finish

--- bookers2-task-5/app/controllers/bookcomments_controller.rb
+++ bookers2-task-5/app/controllers/bookcomments_controller.rb
@@ -1,17 +1,15 @@
 class BookcommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = Bookcomment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    #redirect_to book_path(book)
   end
 
   def destroy
     Bookcomment.find(params[:id]).destroy
-    book = Book.find(params[:book_id])
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
   end
 
 
--- bookers2-task-5/app/controllers/favorites_controller.rb
+++ bookers2-task-5/app/controllers/favorites_controller.rb
@@ -3,12 +3,13 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
+    render :favorite
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
+    render :favorite
   end
 end
-p
