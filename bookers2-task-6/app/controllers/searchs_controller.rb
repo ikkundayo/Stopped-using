70b9e5cfe407abe02b2f811509e31ea33a90e25f
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-03-01 13:24:58 +0000

    finish-task-7b

--- bookers2-task-6/app/controllers/searchs_controller.rb
+++ bookers2-task-6/app/controllers/searchs_controller.rb
@@ -11,28 +11,37 @@ class SearchsController < ApplicationController
 			@records = Book.search_for(@content, @method)
 		end
 	end
+
   # def search
-  #   @model = params["model"]
-  #   @content = params["content"]
-  #   @method = params["method"]
+  #   @model = params[:model]
+  #   @content = params[:content]
+  #   @method = params[:method]
   #   @records = search_for(@model, @content, @method)
   # end
-  
+
   # private
-  
+
   # def search_for(model, content, method)
   #   if model == 'user'
-  #     if method == 'perfect'
-  #       User.where(name: content)
-  #     else
-  #       User.where('name LIKE ?', '%'+content+'%')
-  #     end
+  #       if method == 'perfect'
+  #         User.where(name: content)
+  #       elsif method == 'forward'
+  #         User.where('name LIKE ?', content + '%')
+  #       elsif method == 'backward'
+  #         User.where('name LIKE ?', '%' + content)
+  #       else
+  #         User.where('name LIKE ?', '%' + content + '%')
+  #       end
   #   elsif model == 'book'
-  #     if method == 'perfect'
-  #       Book.where(title: content)
-  #     else
-  #       Book.where('title LIKE ?', '%'+content+'%')
-  #     end
+  #       if method == 'perfect'
+  #         Book.where(title: content)
+  #       elsif method == 'forward'
+  #         Book.where('title LIKE ?', content + '%')
+  #       elsif method == 'backward'
+  #         Book.where('title LIKE ?', '%' + content)
+  #       else
+  #         Book.where('title LIKE ?', '%' + content + '%')
+  #       end
   #   end
   # end
 end
