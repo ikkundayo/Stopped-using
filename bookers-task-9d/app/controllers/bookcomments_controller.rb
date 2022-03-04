82a8bbb2c70b9584c24268552f6519d20abda2a9
EC2 Default User <ec2-user@ip-172-31-31-134.ec2.internal>
2022-03-04 03:04:59 +0000

    update

---
+++ bookers-task-9d/app/controllers/bookcomments_controller.rb
@@ -0,0 +1,24 @@
+class BookcommentsController < ApplicationController
+  def create
+    @book = Book.find(params[:book_id])
+    comment = Bookcomment.new(book_comment_params)
+    comment.user_id = current_user.id
+    comment.book_id = @book.id
+    comment.save
+  end
+
+  def destroy
+    Bookcomment.find(params[:id]).destroy
+    @book = Book.find(params[:book_id])
+  end
+
+
+
+
+  private
+
+  def book_comment_params
+    params.require(:bookcomment).permit(:comment)
+  end
+
+end
